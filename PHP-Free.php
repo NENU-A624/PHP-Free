20805ceed582419cf553058af3ea02be18021ad9
CloudLake <41881295+STCloudLake@users.noreply.github.com>
2022-11-07 21:56:22 +0800

    0.0.0.10

--- PHP-Free.php
+++ PHP-Free.php
@@ -1,1 +1,32 @@
-123 456
+<!--    Designer:  CloudLake
+        Student ID:2020013396
+
+        Purpose: Class Exercise
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Your company name-About</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> <!-- sets the webpage display to full for all devices-->
+    <link rel="shortcut icon" href="_img/logoicon.png" /> </link>  <!-- web broswer tab logo -->
+    <link rel="stylesheet" type="text/css" href="_css/style.css" /></link>
+    <script type="text/javascript" src="_js/index.js"></script>
+</head>
+<body>
+    <header>
+        <h1>About</h1>
+        <a href="_img/phi.png" target="_blank">
+            <img src="_img/phi.png" alt="phiB" height="300"></img>
+        </a>
+        <nav>
+            <ul>
+                <li><a href="home.html" style="color:#00FFFF">Home</a></li>
+                <li><a href="#" style="color:#000000">About</a></li>
+                <li><a href="service.html" style="color:#FF00FF">Service</a></li>
+            </ul>
+        </nav>
+        <h2>See you</h2>
+    </header>
+</body>
+</html>
